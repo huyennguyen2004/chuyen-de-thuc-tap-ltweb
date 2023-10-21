cbea25f5a35a2cde011cc8f64bd15de80d0527cf
huyennguyen2004 <144600981+huyennguyen2004@users.noreply.github.com>
2023-10-21 18:53:14 +0700

    projectwb

--- views/backend/product/index.php
+++ views/backend/product/index.php
@@ -1,80 +1,84 @@
-<?php 
+<?php
+
 use App\Models\Product;
-$list=Product:: where('status','!=',0)
-->orderby('created_at','DESC')
-->get();
+
+$list = Product::where('status', '!=', 0)
+   ->orderBy('created_at', 'DESC')
+   ->get();
 ?>
 
-<?php require_once '../views/backend/header.php';?>
-      <!-- CONTENT -->
-      <form action="" method="post">
-         <div class="content-wrapper">
-            <section class="content-header">
-               <div class="container-fluid">
-                  <div class="row mb-2">
-                     <div class="col-sm-12">
-                        <h1 class="d-inline">Tất cả sản phẩm</h1>
-                        <a href="product_create.html" class="btn btn-sm btn-primary">Thêm sản phẩm</a>
-                     </div>
-                  </div>
+<?php require_once "../views/backend/header.php"; ?>
+<!-- CONTENT -->
+<form action="" method="post">
+   <div class="content-wrapper">
+      <section class="content-header">
+         <div class="container-fluid">
+            <div class="row mb-2">
+               <div class="col-sm-12">
+                  <h1 class="d-inline">Tất cả sản phẩm</h1>
+                  <a href="product_create.html" class="btn btn-sm btn-primary">Thêm sản phẩm</a>
                </div>
-            </section>
-            <!-- Main content -->
-            <section class="content">
-               <div class="card">
-                  <div class="card-header">
-                     <select name="" id="" class="form-control d-inline" style="width:100px;">
-                        <option value="">Xoá</option>
-                     </select>
-                     <button class="btn btn-sm btn-success">Áp dụng</button>
-                  </div>
-                  <div class="card-body">
-                     <table class="table table-bordered" id="mytable">
-                        <thead>
-                           <tr>
-                              <th class="text-center" style="width:30px;">
-                                 <input type="checkbox">
-                              </th>
-                              <th class="text-center" style="width:130px;">Hình ảnh</th>
-                              <th>Tên sản phẩm</th>
-                              <th>Tên danh mục</th>
-                              <th>Tên thương hiệu</th>
-                           </tr>
-                        </thead>
-                        <tbody>
-
-                        <?php if(count($list)>0):?>
-                              <?php foreach($list as $item):?>
+            </div>
+         </div>
+      </section>
+      <!-- Main content -->
+      <section class="content">
+         <div class="card">
+            <div class="card-header">
+               <select name="" id="" class="form-control d-inline" style="width:100px;">
+                  <option value="">Xoá</option>
+               </select>
+               <button class="btn btn-sm btn-success">Áp dụng</button>
+            </div>
+            <div class="card-body">
+               <table class="table table-bordered" id="mytable">
+                  <thead>
+                     <tr>
+                        <th class="text-center" style="width:30px;">
+                           <input type="checkbox">
+                        </th>
+                        <th class="text-center" style="width:130px;">Hình ảnh</th>
+                        <th>Tên sản phẩm</th>
+                        <th>Tên danh mục</th>
+                        <th>Tên thương hiệu</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <?php if (count($list) > 0) : ?>
+                        <?php foreach ($list as $item) : ?>
                            <tr class="datarow">
                               <td>
                                  <input type="checkbox">
                               </td>
                               <td>
-                                 <img src="../public/images/product.jpg" alt="product.jpg">
+                                 <img src="../public/images/product/<?= $item->image; ?>" alt="<?= $item->image; ?>">
                               </td>
                               <td>
                                  <div class="name">
-                                    Tên sản phẩm
+                                    <?= $item->name; ?>
                                  </div>
                                  <div class="function_style">
-                                    <a href="#">Hiện</a> |
-                                    <a href="#">Chỉnh sửa</a> |
-                                    <a href="product_show.html">Chi tiết</a> |
-                                    <a href="#">Xoá</a>
+                                    <?php if ($item->status == 1) : ?>
+                                       <a class="text-success" href="index.php?option=category&cat=status&id=<?= $item->id; ?>">Hiện</a> |
+                                    <?php else : ?>
+                                       <a class="text-danger" href="index.php?option=category&cat=status&id=<?= $item->id; ?>">Ẩn</a> |
+                                    <?php endif; ?>
+                                    <a href="index.php?option=category&cat=edit&id=<?= $item->id; ?>">Chỉnh sửa</a> |
+                                    <a href="index.php?option=category&cat=show&id=<?= $item->id; ?>">Chi tiết</a> |
+                                    <a href="index.php?option=category&cat=delete&id=<?= $item->id; ?>">Xoá</a>
                                  </div>
                               </td>
-                              <td>Tên danh mục</td>
-                              <td>Tên Thuong hiệu</td>
+                              <td><?= $item->slug; ?></td>
+                              <td><?= $item->slug; ?></td>
                            </tr>
-                           <?php endforeach;?>
-                              <?php endif;?>
-
-                        </tbody>
-                     </table>
-                  </div>
-               </div>
-            </section>
+                        <?php endforeach; ?>
+                     <?php endif; ?>
+                  </tbody>
+               </table>
+            </div>
          </div>
-      </form>
-      <!-- END CONTENT-->
-      <?php require_once '../views/backend/footer.php';?>
+      </section>
+   </div>
+</form>
+<!-- END CONTENT-->
+<?php require_once "../views/backend/footer.php"; ?>
